c84b9dc876072a2ee84287818923975ab6dcf925
END <emanueleglico@gmail.com>
2024-11-09 14:56:48 +0100

    Esercizio Comicon

--- aree.js
+++ aree.js
@@ -1,15 +1,27 @@
 const items = ["Fumetto", "Videogame", "Cinema & Serie TV", "Gioco", "Asian", "Musica", "Cosplay", "Kids", "LARP-Neverland", "Pizzacon"];
 const lista = document.querySelectorAll(".listaAree");
 const tendina = document.getElementById("TendinaAree");
+const aree = document.getElementById("Aree");
 
 for (let i = 0; i < lista.length; i++) {
   lista[i].textContent = items[i];
 }
 
-function dropdownAree() {
+aree.addEventListener("click", dropdownAree);
+
+function dropdownAree(event) {
   if (tendina.style.visibility === "hidden") {
     tendina.style.visibility = "visible";
-  }else{
+    tendina.style.opacity = "1"
+  } else {
     tendina.style.visibility = "hidden";
+    tendina.style.opacity = "0"
   }
 }
+
+window.addEventListener("click", function (event) {
+  if (!event.target.matches("#Aree")) {
+    tendina.style.visibility = "hidden";
+    tendina.style.opacity = "0"
+  }
+});
