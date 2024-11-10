5a30f00fa0b3d952da281a85e32e7de2c8703ca4
END <emanueleglico@gmail.com>
2024-11-10 16:43:31 +0100

    Esercizio Comicon

---
+++ attivita.js
@@ -0,0 +1,27 @@
+const items1 = ["Mostre","Festival","Editoria","Agenzia artisti"];
+const lista2 = document.querySelectorAll(".listaAttivita");
+const tendina2 = document.getElementById("TendinaAttivita");
+const attivita = document.getElementById("Attivita");
+
+for (let i = 0; i < lista2.length; i++) {
+  lista2[i].textContent = items1[i];
+}
+
+attivita.addEventListener("click",dropdownAttivita)
+
+function dropdownAttivita() {
+  if (tendina2.style.visibility === "hidden") {
+    tendina2.style.visibility = "visible";
+    tendina2.style.opacity = "1";
+  } else {
+    tendina2.style.visibility = "hidden";
+    tendina2.style.opacity = "0";
+  }
+}
+
+window.addEventListener("click",function(event){
+  if(!event.target.matches("#Attivita")){
+    tendina2.style.visibility = "hidden";
+    tendina2.style.opacity = "0";
+  }
+});
