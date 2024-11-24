0eede5959c09060cc0711c4a995b9562728abc0d
END <emanueleglico@gmail.com>
2024-11-24 13:18:14 +0100

    Esercizio Comicon

--- scripts/aree.js
+++ scripts/aree.js
@@ -1,29 +1,36 @@
 const items = ["Fumetto", "Videogame", "Cinema & Serie TV", "Gioco", "Asian", "Musica", "Cosplay", "Kids", "LARP-Neverland", "Pizzacon"];
 const lista = document.querySelectorAll(".listaAree");
-const tendina = document.getElementById("TendinaAree");
-tendina.style.visibility = "hidden";
+
+const items2 = ["Mostre", "Festival", "Editoria", "Agenzia artisti"];
+const lista2 = document.querySelectorAll(".listaAttivita");
+
+const tendina = document.querySelectorAll(".tendine");
+tendina.style.display = "none";
 const aree = document.getElementById("Aree");
 
 for (let i = 0; i < lista.length; i++) {
   lista[i].textContent = items[i];
 }
 
+for (let i = 0; i < lista2.length; i++) {
+  lista2[i].textContent = items1[i];
+}
+
 aree.addEventListener("click", dropdownAree);
 
 function dropdownAree(event) {
-  if (tendina.style.visibility === "hidden") {
+  if (tendina.style.display === "none") {
     tendina.style.display = "block"
-    tendina.style.visibility = "visible";
     tendina.style.opacity = "1"
   } else {
-    tendina.style.visibility = "hidden";
+    tendina.style.display = "none";
     tendina.style.opacity = "0"
   }
 }
 
 window.addEventListener("click", function (event) {
   if (!event.target.matches("#Aree")) {
-    tendina.style.visibility = "hidden";
+    tendina.style.display = "none";
     tendina.style.opacity = "0"
   }
 });
--- scripts/attivita.js
+++ scripts/attivita.js
@@ -1,12 +1,9 @@
-const items1 = ["Mostre","Festival","Editoria","Agenzia artisti"];
-const lista2 = document.querySelectorAll(".listaAttivita");
+
 const tendina2 = document.getElementById("TendinaAttivita");
 tendina2.style.visibility = "hidden";
 const attivita = document.getElementById("Attivita");
 
-for (let i = 0; i < lista2.length; i++) {
-  lista2[i].textContent = items1[i];
-}
+
 
 attivita.addEventListener("click",dropdownAttivita)
 
